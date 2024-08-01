0ecbf912182af686c2f703629d14b16a4082de2f
cryptical-coder-1 <kirambawycliff@gmail.com>
2024-08-02 00:06:31 +0300

    my file

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,23 @@
+
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map(project => (
+          <ProjectItem
+            key={project.id} // Unique key for each ProjectItem
+            name={project.name} // Pass project name
+            about={project.description} // Pass project description (renamed to "about")
+            technologies={project.technologies || []} // Pass technologies, default to empty array
+          />
+        ))}
+      </div>
     </div>
   );
 }
